f85bd371f0ed9e18099ce7faf8aba0edb2423b91
UminKim <kimym9603@naver.com>
2017-05-24 21:42:24 +0900

    IntroActivity추가
    ㅊㄱㅊㄱ

---
+++ app/src/main/java/com/example/kimym/mobileproject/IntroActivity.java
@@ -0,0 +1,24 @@
+package com.example.kimym.mobileproject;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        Handler handler = new Handler();
+        handler.postDelayed(new splashhandler(), 3000);
+    }
+    private class splashhandler implements Runnable{
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class)); // 로딩이 끝난후 이동할 Activity
+            IntroActivity.this.finish(); // 로딩페이지 Activity Stack에서 제거
+        }
+    }
+
+}
--- app/src/main/java/com/example/kimym/mobileproject/MainActivity.java
+++ app/src/main/java/com/example/kimym/mobileproject/MainActivity.java
@@ -3,13 +3,9 @@ package com.example.kimym.mobileproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.firebase.database.FirebaseDatabase;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
@@ -28,20 +24,11 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.text);
         textView = (TextView)findViewById(R.id.textView);
     }
-
     public void Search(View view) {
         String url = editText.getText().toString();
-//
-//        WebView webView = (WebView)findViewById(R.id.webView);
-//        webView.setWebViewClient(new WebViewClient());
-//        WebSettings settings = webView.getSettings();
-//        settings.setJavaScriptEnabled(true);
-//        settings.setBuiltInZoomControls(true);
-//        settings.setDefaultTextEncodingName("utf-8");
-//        webView.loadUrl(url);
 
         Ion.with(this)
-                .load("http://openapi.seoul.go.kr:8088/727a4e74456b696d36336278547079/json/SearchPublicToiletPOIService/1/5/")
+                .load("[")
                 .asString()
                 .setCallback(new FutureCallback<String>() {
                     @Override
@@ -52,27 +39,25 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
     public void parsingJSON(String result){
-        //textView.setText(result);
         String total = "";
         try {
             JSONObject json = new JSONObject(result);
             JSONObject object = json.getJSONObject("SearchPublicToiletPOIService");
             JSONArray array = object.getJSONArray("row");
             for(int i = 0; i < array.length(); i++){
-                String POI_ID = array.getJSONObject(i).getString("POI_ID");
-                String FNAME = array.getJSONObject(i).getString("FNAME");
-                String ANAME = array.getJSONObject(i).getString("ANAME");
-                String CNAME = array.getJSONObject(i).getString("CNAME");
-                String CENTER_X1 = array.getJSONObject(i).getString("CENTER_X1");
-                String CENTER_Y1 = array.getJSONObject(i).getString("CENTER_Y1");
-                String X_WGS84 = array.getJSONObject(i).getString("X_WGS84");
-                String Y_WGS84 = array.getJSONObject(i).getString("Y_WGS84");
-                String INSERTDATE = array.getJSONObject(i).getString("INSERTDATE");
-                String UPDATEDATE = array.getJSONObject(i).getString("UPDATEDATE");
+                String POI_ID = array.getJSONObject(i).getString("POI_ID"); // POI ID
+                String FNAME = array.getJSONObject(i).getString("FNAME"); // 대명칭
+                String ANAME = array.getJSONObject(i).getString("ANAME"); // 중명칭
+                String CNAME = array.getJSONObject(i).getString("CNAME"); // 소명칭
+                String CENTER_X1 = array.getJSONObject(i).getString("CENTER_X1"); // 중앙좌표 X1
+                String CENTER_Y1 = array.getJSONObject(i).getString("CENTER_Y1"); // 중앙좌표 Y1
+                String X_WGS84 = array.getJSONObject(i).getString("X_WGS84"); // WSG84X좌표
+                String Y_WGS84 = array.getJSONObject(i).getString("Y_WGS84"); // WSG84Y좌표
+                String INSERTDATE = array.getJSONObject(i).getString("INSERTDATE"); // 등록일자
+                String UPDATEDATE = array.getJSONObject(i).getString("UPDATEDATE"); // 수정일자
 
                 total += FNAME + "\t" + X_WGS84 + "\t" + Y_WGS84 + "\n";
             }
-
             textView.setText(total);
         } catch (JSONException e) {
             e.printStackTrace();
